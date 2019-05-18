084d1ecb0581fef87d74b3fbbb69df40c3f40abc
aydnalpay <50770344+aydnalpay@users.noreply.github.com>
2019-05-18 15:01:01 +0300

    Add files via upload

---
+++ SuOtomasyonu/src/suotomasyonu/Database.java
@@ -0,0 +1,16 @@
+// Databasemizin özelliklerinin tutulduğu bilgiler burada olacak.
+// Bilgiler statick ve final olarak tanımlanacak ve bilgilerin değiştirilmesi engellenecek.
+package suotomasyonu;
+
+
+public class Database {
+    
+
+    public static final String db_ismi = "susatis";
+    
+    public static final String host =  "localhost";
+    
+    public static final int port = 3306;
+    
+}
+
